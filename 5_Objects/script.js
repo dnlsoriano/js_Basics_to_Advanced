11ea3b8e201fb1dac9d62f096f107295ea33da9d
dsoriano <d.soriano91@gmail.com>
2022-06-13 10:53:07 +0200

    Constructor Functions

--- 5_Objects/script.js
+++ 5_Objects/script.js
@@ -25,6 +25,7 @@ var video = {
     duration : "3:20",
     owner : "Dsoriano",
     getVideoName: function() {
+        // this keyword referes to the current context, always refers to a single object
         return this.videoName;
     },
     getFileFormat: function() {
@@ -39,3 +40,20 @@ var video = {
 }
 
 console.log(video.getVideoName());
+
+//Constructor function
+function Video(name, format, duration, owner) {
+    this.videoName = name;
+    this.fileFormat = format;
+    this.duration = duration;
+    this.owner = owner;
+
+    this.presentation = function() {
+        console.log(this.videoName + " directed by " + this.owner);
+    }
+}
+
+var newVideo = new Video("test", "mp4", "3.13", "tester");
+
+console.log(newVideo);
+console.log(newVideo.presentation());
