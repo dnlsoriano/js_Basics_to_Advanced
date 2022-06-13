00901a7c41b9358284191c48921e8633b8049eb0
dsoriano <d.soriano91@gmail.com>
2022-06-13 13:09:27 +0200

    Starting ES6

---
+++ 8_ES6/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    // 2 new ways to create variables
+    // let -> replacement for var
+    // const  -> to create constants, variables created this way can't be updated later in the code
+});
