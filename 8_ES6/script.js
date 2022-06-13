ed6709c0cbe8330525b1875893051420f576b078
dsoriano <d.soriano91@gmail.com>
2022-06-13 13:15:31 +0200

    added template strings

--- 8_ES6/script.js
+++ 8_ES6/script.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
     // 2 new ways to create variables
     // let -> replacement for var
     // const  -> to create constants, variables created this way can't be updated later in the code
+
+    // Template Strings
+    // It's a string which allows embedding expressions inside it
+    // eg const greetings = `Hello ${name}`
+
+    const firstName = 'John';
+    const lastName = 'Stevens';
+
+    function getFullName(firstName, lastName) {
+        return `${firstName} ${lastName}`;
+    };
+
+    console.log(`Hello ${getFullName(firstName, lastName)}`);
 });
